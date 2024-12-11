0cedc0e150a8e8e8057cd8f46b7dae49a83e8cde
Bobomurod Amirov <bobomurodamirov.b@gmail.com>
2024-12-11 19:55:58 +0500

    Added log out

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -19,6 +19,7 @@
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="/todos">To-do List</a></li>
                             <li><a class="dropdown-item" href="/todos"><?= $_SESSION['user']['full_name'] ?? '' ?></a></li>
+                            <li><a class="dropdown-item" href="/log_out">Log out</a></li>
                         </ul>
                     </li>
                 <?php else: ?>
