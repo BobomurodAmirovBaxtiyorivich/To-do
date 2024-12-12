2ee5b56d98a6da79548e54a701c77f0416bd732a
Abdulaziz <abdulazizsalihov08@gmail.com>
2024-12-12 14:38:40 +0500

    navbar updated

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -19,6 +19,8 @@
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="/todos">To-do List</a></li>
                             <li><a class="dropdown-item" href="/todos"><?= $_SESSION['user']['full_name'] ?? '' ?></a></li>
+                            <li><a class="dropdown-item" href="/todos"><?= $_SESSION['user']['email'] ?? '' ?></a></li>
+
                             <li><a class="dropdown-item" href="/logout">Log out</a></li>
                         </ul>
                     </li>
