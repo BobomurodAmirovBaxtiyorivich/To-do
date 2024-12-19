39dcbb78cab094fa789c3ed503697d147450c4c4
nodirbekerkabayev <erkabayevnodirbek@gmail.com>
2024-12-19 15:59:04 +0500

    added code

--- src/Bot.php
+++ src/Bot.php
@@ -34,7 +34,6 @@ class Bot {
             $tasksLIst .= $task['due_date'] . "\n";
             $tasksLIst .= $task['status'] . "\n\n";
             $tasksLIst .= "===========================\n\n";
-
         }
         return $tasksLIst;
     }
